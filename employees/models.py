643d2055e163a0a921a21cd4cfee771cb1446db2
aliaksandr_tsiustsin <aliaksandr_tsiustsin@epam.com>
2022-03-24 23:53:51 +0300

    Added validators to save method

--- employees/models.py
+++ employees/models.py
@@ -20,12 +20,17 @@ class Employee(models.Model):
 
 
 class Relation(MPTTModel):
-    name = models.ForeignKey(Employee, verbose_name="Сотрудник", on_delete=models.CASCADE)
-    parent = TreeForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children',
-                            verbose_name="Руководитель")
-
-    def __str__(self):
-        return f"{self.name}"
+    name = models.ForeignKey(Employee,
+                             verbose_name="Сотрудник",
+                             on_delete=models.CASCADE,
+                             )
+    parent = TreeForeignKey('self',
+                            on_delete=models.SET_NULL,
+                            null=True,
+                            blank=True,
+                            related_name='children',
+                            verbose_name="Руководитель",
+                            )
 
     class MPTTMeta:
         level = 'mptt_level'
@@ -34,6 +39,34 @@ class Relation(MPTTModel):
     class Meta:
         db_table = "relations"
         verbose_name_plural = "Отношения"
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def validate_max_indent(self):
+        max_indent = 5
+        lvl = self.parent.level
+        if lvl >= max_indent - 1:
+            raise ValueError(f"Максимальная вложенность: {max_indent}")
+
+    def validate_employee_is_equal_lead(self):
+        if self.parent.name == self.name:
+            raise ValueError(f"Сотрудник не может подчиняться самому себе!")
+
+    def validate_is_employee_lead(self):
+        try:
+            Relation.objects.get(name=self.name).name
+        except Relation.DoesNotExist:
+            pass
+        else:
+            raise ValueError(f"У сотрудника может быть только один руководитель!")
+
+    def save(self, *args, **kwargs):
+        if self.parent is not None:
+            self.validate_max_indent()
+            self.validate_employee_is_equal_lead()
+        self.validate_is_employee_lead()
+        super().save(*args, **kwargs)
 
 
 mptt.register(Relation, order_insertion_by=['name'])
