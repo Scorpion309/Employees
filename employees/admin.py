bfeb31123b8f5a77d63b079fe087cd4457fad852
aliaksandr_tsiustsin <aliaksandr_tsiustsin@epam.com>
2022-03-25 10:27:16 +0300

    Added level filter

--- employees/admin.py
+++ employees/admin.py
@@ -7,19 +7,14 @@ from .models import Employee, Relation
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('name', 'position', 'get_lead', 'monthly_salary', 'paid_salary')
-    list_filter = ('position',)
+    list_filter = ('position', 'relation__level')
 
     def get_lead(self, obj):
         lead = Relation.objects.get(name=obj).parent
         lead_id = Employee.objects.get(name=lead).id
-        return format_html(f"<a href={lead_id}>{lead}</a>")
+        return format_html(f'<a href={lead_id}>{lead}</a>')
 
     get_lead.short_description = u'Руководитель'
-
-    def get_level(self, obj):
-        return Relation.objects.get(name=obj).level
-
-    get_level.short_description = u'Уровень иерархии'
 
 
 @admin.register(Relation)
--- employees/models.py
+++ employees/models.py
@@ -4,24 +4,24 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Employee(models.Model):
-    name = models.CharField("ФИО", max_length=32)
-    position = models.CharField("Должность", max_length=32)
-    employment_date = models.DateTimeField("Дата приема на работу", )
-    monthly_salary = models.IntegerField("Заработная плата", default=0)
-    paid_salary = models.IntegerField("Всего выплачено", default=0)
+    name = models.CharField('ФИО', max_length=32)
+    position = models.CharField('Должность', max_length=32)
+    employment_date = models.DateTimeField('Дата приема на работу', )
+    monthly_salary = models.IntegerField('Заработная плата', default=0)
+    paid_salary = models.IntegerField('Всего выплачено', default=0)
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
     class Meta:
-        db_table = "employees"
-        verbose_name = "Сотрудник"
-        verbose_name_plural = "Сотрудники"
+        db_table = 'employees'
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
 
 
 class Relation(MPTTModel):
     name = models.ForeignKey(Employee,
-                             verbose_name="Сотрудник",
+                             verbose_name='Сотрудник',
                              on_delete=models.CASCADE,
                              )
     parent = TreeForeignKey('self',
@@ -29,7 +29,7 @@ class Relation(MPTTModel):
                             null=True,
                             blank=True,
                             related_name='children',
-                            verbose_name="Руководитель",
+                            verbose_name='Руководитель',
                             )
 
     class MPTTMeta:
@@ -37,21 +37,21 @@ class Relation(MPTTModel):
         order_insertion_by = ['name']
 
     class Meta:
-        db_table = "relations"
-        verbose_name_plural = "Отношения"
+        db_table = 'relations'
+        verbose_name_plural = 'Отношения'
 
     def __str__(self):
-        return f"{self.name}"
+        return f'{self.name}'
 
     def validate_max_indent(self):
         max_indent = 5
         lvl = self.parent.level
         if lvl >= max_indent - 1:
-            raise ValueError(f"Максимальная вложенность: {max_indent}")
+            raise ValueError(f'Максимальная вложенность: {max_indent}')
 
     def validate_employee_is_equal_lead(self):
         if self.parent.name == self.name:
-            raise ValueError(f"Сотрудник не может подчиняться самому себе!")
+            raise ValueError(f'Сотрудник не может подчиняться самому себе!')
 
     def validate_is_employee_lead(self):
         try:
@@ -59,7 +59,7 @@ class Relation(MPTTModel):
         except Relation.DoesNotExist:
             pass
         else:
-            raise ValueError(f"У сотрудника может быть только один руководитель!")
+            raise ValueError(f'У сотрудника может быть только один руководитель!')
 
     def save(self, *args, **kwargs):
         if self.parent is not None:
