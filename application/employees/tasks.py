ef41c43befaf5b552f60054f4e364cec968ebcc5
aliaksandr_tsiustsin <aliaksandr_tsiustsin@epam.com>
2022-03-28 21:05:18 +0300

    Added pay every 2 hours task

--- application/employees/tasks.py
+++ application/employees/tasks.py
@@ -1,3 +1,4 @@
+from celery.schedules import crontab
 from celery.utils.log import get_task_logger
 from staff.celery import app
 
@@ -6,9 +7,18 @@ from .models import Employee
 logger = get_task_logger(__name__)
 
 
-# @app.task
-# def task_pay_salary():
-#     logger.info(f"Salary paid!")
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(crontab(hour='*/2'), task_pay_salary.s(), name='Pay_every_2_hours')
+
+
+@app.task
+def task_pay_salary():
+    employee_objects = Employee.objects.all()
+    for employee in employee_objects:
+        Employee.objects.filter(name=employee.name).update(paid_salary=employee.monthly_salary + employee.paid_salary)
+    logger.info(f"Salary for employees successfully payed!")
+
 
 @app.task
 def async_delete_paid_salary(employees):
