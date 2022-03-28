14caf54a40dde63c5530f2376c375a6e8f9bd7bb
aliaksandr_tsiustsin <aliaksandr_tsiustsin@epam.com>
2022-03-28 16:59:34 +0300

    Added celery and redis

--- application/staff/settings.py
+++ application/staff/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'employees.Employee'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
